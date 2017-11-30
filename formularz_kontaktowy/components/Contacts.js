1c4116a6489798a8d6d40e1a2bfe21ebdf47a0c2
Jacek Achtelik <jacekachtelik@gmail.com>
2017-11-30 20:10:52 +0100

    Mod 02 Task 05

---
+++ formularz_kontaktowy/components/Contacts.js
@@ -0,0 +1,15 @@
+var Contacts = React.createClass({
+    propTypes: {
+        items: React.PropTypes.array.isRequired
+    },
+    
+    render: function () {
+        var contacts = this.props.items.map(function (contact) {
+            console.log(contacts);
+            return React.createElement(Contact, {item: contact, key: contact.id});
+        }); // Koniec map
+        return (
+            React.createElement('li',{className: 'contactList'},contacts)
+        );
+    } // Koniec render
+}); // Koniec klasy Contacts
