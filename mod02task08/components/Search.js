dd5158d2103c9b19fff10db1709d59d5f71bf741
Jacek Achtelik <jacekachtelik@gmail.com>
2017-12-11 21:05:27 +0100

    m02t08 errors

--- mod02task08/components/Search.js
+++ mod02task08/components/Search.js
@@ -1,4 +1,9 @@
 Search = React.createClass({
+    getInitialState() {
+        return {
+          searchingText: ''
+        };
+      },
     handleChange: function(event) {
         var searchingText = event.target.value;
         this.setState({searchingText: searchingText});
